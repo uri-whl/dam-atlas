e4b78e86989ebb3a8e531d22129496c55bd0e582
Josh P. Sawyer <josh.p.sawyer@gmail.com>
2020-02-02 16:47:36 -0500

    script to restrict to certain watersheds + state of RI

---
+++ src/cut_dam_dataset_to_ri.py
@@ -0,0 +1,64 @@
+# -*- coding: utf-8 -*-
+"""
+Created on 2020-01-16
+
+@author: Josh P. Sawyer
+"""
+
+import sys
+import os.path
+import extarc as ea
+
+import arcpy
+from pyprojroot import here
+
+# set environment flags - we don't want Z / M, we do want overwrite on
+# the final product
+arcpy.env.outputZFlag = "Disabled"
+arcpy.env.outputMFlag = "Disabled"
+arcpy.env.overwriteOutput = True
+
+arcpy.env.scratchWorkspace = str(here('./results/scratch'))
+
+if __name__ == "__main__":
+    ea.logger.setup_logging(here("./src/logging.yaml"))
+    ea.logger.send("associating dams with huc12")
+    
+    all_dams_hucs = str(here('./results/results.gdb/merged_dams_hucs', warn=False))
+    
+    # make flayer, select only dams in RI OR in wsheds of interest, make copy
+    
+    dams_flayer = "dams_flayer"
+    
+    arcpy.MakeFeatureLayer_management(all_dams_hucs, dams_flayer)
+
+    arcpy.SelectLayerByAttribute_management(
+        dams_flayer,
+        'NEW_SELECTION',
+        "STATE = 'RI'"
+    )
+    
+    arcpy.SelectLayerByAttribute_management(
+        dams_flayer,
+        'ADD_TO_SELECTION',
+        "HUC8 = '01090004'",
+        ""
+    )
+
+    arcpy.SelectLayerByAttribute_management(
+        dams_flayer,
+        'ADD_TO_SELECTION',
+        "HUC8 = '01090005'",
+        ""
+    )
+    
+    arcpy.SelectLayerByAttribute_management(
+        dams_flayer,
+        'ADD_TO_SELECTION',
+        "HUC8 = '01090003'"
+    )
+    
+    # save to results folder
+    output_loc = str(here('./results/results.gdb/aoi_dams_hucs', warn=False))
+    
+    arcpy.CopyFeatures_management(dams_flayer, output_loc)
