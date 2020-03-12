d9c8bd683404d1cc62afc127dd469ead72227714
Josh P. Sawyer <josh.p.sawyer@gmail.com>
2020-03-11 22:34:37 -0400

    updated dam snapping script

--- src/02_snap_dams_to_nhdphr.py
+++ src/02_snap_dams_to_nhdphr.py
@@ -5,16 +5,11 @@ Created on 2020-01-16
 @author: Josh P. Sawyer
 """
 
-import sys
 import os.path
 
-# self append to grab arcutils module
-sys.path.append(os.path.dirname(__file__))
-
+import extarc as ea
 import arcpy
-from pyprojroot import here
-from arcutils import arcutils as au
-from arcutils.common_logger import setup_logging
+from pyhere import here
 import logging
 
 # set environment flags - we don't want Z / M, we do want overwrite on
@@ -42,31 +37,29 @@ nhd_hucs = [
 arcpy.env.scratchWorkspace = str(here('./results/scratch'))
 
 if __name__ == "__main__":
-    # create a logger
-    setup_logging()
+    ea.logger.setup_logging(here("src", "logging.yaml"))
     logger = logging.getLogger(__name__)
-    
+   
     logger.info("Snapping dams to NHD HR Flowline")
 
     # starting with combined dataset
-    merged_dams = str(here('./results/results.gdb/merged_dams', warn=False))
+    merged_dams = str(here("results", "results.gdb", "merged_dams"))
     
     # make a copy - snap edits in place
-    dam_copy = au.get_unused_scratch_fc()
+    dam_copy = ea.obj.get_unused_scratch_gdb_obj()
     arcpy.CopyFeatures_management(merged_dams, dam_copy)
            
     flowlines = []
     
     # snap to flowline of nhd - there are 10 nhdplus datasets so we combine them
     for huc in nhd_hucs:
-        nhd_loc = str(here('./data/nhdplus_h/'))
+        nhd_loc = here("data", "nhdplus_h")
         nhd_gdb = nhd_gdb_p + huc + nhd_gdb_s
-        flow_path = 'Hydrography' + os.path.sep + 'NHDFlowline'
-        flowline = os.path.join(nhd_loc, nhd_gdb, flow_path)
+        flowline = nhd_loc / nhd_gdb / "Hydrography" / "NHDFlowline"
+        
+        flowlines.append(str(flowline))
     
-        flowlines.append(flowline)
-    
-    m_flowline = au.get_unused_scratch_fc()
+    m_flowline = ea.obj.get_unused_scratch_gdb_obj()
     arcpy.Merge_management(flowlines, m_flowline)
     
     # the datasets in consideration are huge - but we can simplify them, as not
@@ -89,22 +82,25 @@ if __name__ == "__main__":
                "WITHIN_A_DISTANCE", dam_sub_lyr, subset_distance,
                "NEW_SELECTION", "NOT_INVERT")
 
-    dam_sub = au.get_unused_scratch_fc()
+    dam_sub = ea.obj.get_unused_scratch_gdb_obj()
     arcpy.CopyFeatures_management(dam_sub_lyr, dam_sub)
     
-    flow_sub = au.get_unused_scratch_fc()
+    flow_sub = ea.obj.get_unused_scratch_gdb_obj()
     arcpy.CopyFeatures_management(flow_sub_lyr, flow_sub)
 
     # data's subsetted, now snap
     arcpy.Snap_edit(dam_sub, [[flow_sub, "EDGE", snap_distance]])
     
     # interesect with flowline to get only those that were snapped
-    dam_snapped = str(here('./results/results.gdb/all_snapped_dams', warn=False))
+    dam_snapped = str(here("results", "results.gdb", "all_snapped_dams"))
     
     arcpy.Intersect_analysis([dam_sub, flow_sub],
                              dam_snapped)
 
     # add new x / y & rename to sane names
-    arcpy.AddXY_management(merged_dams)
-    arcpy.AlterField_management(merged_dams, "POINT_X", "LONGITUDE", "LONGITUDE")
-    arcpy.AlterField_management(merged_dams, "POINT_Y", "LATITUDE", "LATITUDE")
+    arcpy.AddXY_management(dam_snapped)
+    arcpy.AlterField_management(dam_snapped, "POINT_X", "LONGITUDE", "LONGITUDE")
+    arcpy.AlterField_management(dam_snapped, "POINT_Y", "LATITUDE", "LATITUDE")
+    
+    logger.info("finished snapping dams")
+    logger.info("results in {}".format(merged_dams))
