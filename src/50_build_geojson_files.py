741553d5f3b235cf59cef992ff0f59d9d1325427
Josh P. Sawyer <josh.p.sawyer@gmail.com>
2020-03-11 22:50:32 -0400

    updated geojson building script

--- src/50_build_geojson_files.py
+++ src/50_build_geojson_files.py
@@ -5,11 +5,10 @@ Simply converts all of the generated data to geoJSON for use in MapBox.
 @author: Josh P. Sawyer
 """
 
-import os.path
-
 import arcpy
-from pyprojroot import here
+from pyhere import here
 import logging
+import extarc as ea
 
 # set environment flags - we don't want Z / M, we do want overwrite on
 # the final product
@@ -17,23 +16,24 @@ arcpy.env.outputZFlag = "Disabled"
 arcpy.env.outputMFlag = "Disabled"
 arcpy.env.overwriteOutput = True
 
-arcpy.env.scratchWorkspace = str(here('./results/scratch'))
+arcpy.env.scratchWorkspace = str(here("results", "scratch"))
 
 if __name__ == "__main__":
+    ea.logger.setup_logging(here("src", "logging.yaml"))
     logger = logging.getLogger(__name__)
 
     logger.info("Building geoJSON files")
     
     logger.info("Building dam file")
     # the location of our snapped dam data
-    dams = str(here('./results/results.gdb/snapped_aoi_dams', warn=False))
+    dams = str(here("results", "results.gdb", "snapped_aoi_dams"))
     
-    output_dir = str(here('./results/', warn=False))
+    output_dir = here("results")
     
     # output to geojson
     arcpy.FeaturesToJSON_conversion(
         dams,
-        os.path.join(output_dir, "dams_gj.geojson"),
+        str(output_dir / "dams_gj.geojson"),
         "FORMATTED",
         "NO_Z_VALUES",
         "NO_M_VALUES",
